a5b149e00f2ef75d8901acd8ceb7d594e8de7058
CassV22 <cassandravalcourt@comcast.net>
2023-03-22 19:05:48 -0400

    Code works for rock button - duplicate for others; change image divs into one background so response screens work

---
+++ script.js
@@ -0,0 +1,30 @@
+//definition of all relevant variables
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+let n = Math.random();
+let computerChoice = Math.floor(n*3) + 1 ; 
+
+//to change screen based on win, lose, tie
+var screen = document.getElementById("game_screen");
+//to change response based on win, lose, tie
+var feedback = document.getElementById("response");
+
+
+//add an event listener to each button; call comparison function on click and change screen accrodingly
+
+var rock_btn = document.getElementById("rock_btn");
+rock_btn.addEventListener("click", function(){
+    //if statements here and screen changes
+    if (computerChoice == 1) {
+        screen.style.backgroundImage = "url('images/tie.png)";
+        feedback.innerHTML = "It's a tie. Reload the page and try again.";        
+    } else if (computerChoice == 2) {
+        screen.style.backgroundImage = "url('images/loser.gif)";
+        feedback.innerHTML = "You lose. Paper covers rock.";  
+    } else {
+        screen.style.backgroundImage = "url('images/winner.gif)";
+        feedback.innerHTML = "You win! Rock beats scissors.";
+    }
+
+});

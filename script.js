a9d391fac84cf3b0de1f78d213a52cb00b9f8b21
CassV22 <cassandravalcourt@comcast.net>
2023-03-24 23:36:15 -0400

    Completed game with proper interface and JavaScript

--- script.js
+++ script.js
@@ -9,13 +9,63 @@ let computerChoice = Math.floor(n*3) + 1 ;
 var screen = document.getElementById("game_screen");
 //to change response based on win, lose, tie
 var feedback = document.getElementById("response");
+//to hide buttons when response screen appears
+let buttons = document.querySelectorAll(".choice_btn");
 
 
-//add an event listener to each button; call comparison function on click and change screen accrodingly
-
+//function for computer response when user chooses rock
 var rock_btn = document.getElementById("rock_btn");
 rock_btn.addEventListener("click", function(){
-    //if statements here and screen changes
+    //hides choice buttons when response screen is displayed   
+    for (i=0; i<buttons.length; i++){
+        buttons[i].style.display = "none";
+    }
+    
+    //comparison to determines who won
+    if (computerChoice == 1) {
+        screen.src = 'images/tie.png';
+        feedback.innerHTML = "It's a tie. Reload the page and try again.";        
+    } else if (computerChoice == 2) {
+        screen.src = 'images/loser.gif';
+        feedback.innerHTML = "You lose. Paper covers rock.";  
+    } else {
+        screen.src = 'images/winner.gif';
+        feedback.innerHTML = "You win! Rock beats scissors.";
+    }
+
+});
+
+//function for computer response when user chooses paper
+var paper_btn = document.getElementById("paper_btn");
+paper_btn.addEventListener("click", function(){
+    //hides choice buttons when response screen is displayed   
+    for (i=0; i<buttons.length; i++){
+        buttons[i].style.display = "none";
+    }
+    
+    //comparison to determines who won
+    if (computerChoice == 1) {
+        screen.src = 'images/tie.png';
+        feedback.innerHTML = "It's a tie. Reload the page and try again.";        
+    } else if (computerChoice == 2) {
+        screen.src = 'images/loser.gif';
+        feedback.innerHTML = "You lose. Paper covers rock.";  
+    } else {
+        screen.src = 'images/winner.gif';
+        feedback.innerHTML = "You win! Rock beats scissors.";
+    }
+
+});
+
+//function for computer response when user chooses scissors
+var scissors_btn = document.getElementById("scissors_btn");
+scissors_btn.addEventListener("click", function(){
+    //hides choice buttons when response screen is displayed    
+    for (i=0; i<buttons.length; i++){
+        buttons[i].style.display = "none";
+    }
+    
+    //comparison to determines who won
     if (computerChoice == 1) {
         screen.src = 'images/tie.png';
         feedback.innerHTML = "It's a tie. Reload the page and try again.";        

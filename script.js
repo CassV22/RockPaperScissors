ddc89ee84def2d87edc03b254e94b6a322b0304d
CassV22 <cassandravalcourt@comcast.net>
2023-04-21 11:20:28 -0400

    Buttons functioning; need to fix scoring and round conuts

--- script.js
+++ script.js
@@ -4,77 +4,109 @@ const paper = 2;
 const scissors = 3;
 let n = Math.random();
 let computerChoice = Math.floor(n*3) + 1 ; 
+let changeFeedback = document.getElementById("feedback_text");
+let roundNum = document.getElementById("roundNum");
+let computerScore = document.getElementById("computer_score");
+let playerScore = document.getElementById("player_score");
+let buttons = document.querySelectorAll("button");
 
-//to change screen based on win, lose, tie
-var screen = document.getElementById("game_screen");
-//to change response based on win, lose, tie
-var feedback = document.getElementById("response");
-//to hide buttons when response screen appears
-let buttons = document.querySelectorAll(".choice_btn");
-
-
-//function for computer response when user chooses rock
-var rock_btn = document.getElementById("rock_btn");
-rock_btn.addEventListener("click", function(){
-    //hides choice buttons when response screen is displayed   
-    for (i=0; i<buttons.length; i++){
-        buttons[i].style.display = "none";
+let count = 1;
+buttons.forEach(button => button.addEventListener("click", function(){
+    let playerChoice = this.getAttribute('id');
+    
+    if (playerChoice == "rock_btn") {
+        playRock();
+        console.log("Rock function called.");
+    } else if (playerChoice == "paper_btn") {
+        playPaper();
+        console.log("Paper function called.");
+    } else if (playerChoice == "scissors_btn") {
+        playScissor();
+        console.log("Scissors function called.");
     }
     
-    //comparison to determines who won
+    console.log(computerChoice);
+ }));
+
+
+
+
+//add point according to who won
+function playRock(computerChoice) {
     if (computerChoice == 1) {
-        screen.src = 'images/tie.png';
-        feedback.innerHTML = "It's a tie. Reload the page and try again.";        
+        changeFeedback.innerHTML = "It's a tie. No points awarded.";        
     } else if (computerChoice == 2) {
-        screen.src = 'images/loser.gif';
-        feedback.innerHTML = "You lose. Paper covers rock.";  
+        computerScore += 1;
+        changeFeedback.innerHTML = "You lose. Paper covers rock.";  
     } else {
-        screen.src = 'images/winner.gif';
-        feedback.innerHTML = "You win! Rock beats scissors.";
+        changeFeedback.innerHTML = "You win! Rock beats scissors.";
     }
+}
 
-});
-
-//function for computer response when user chooses paper
-var paper_btn = document.getElementById("paper_btn");
-paper_btn.addEventListener("click", function(){
-    //hides choice buttons when response screen is displayed   
-    for (i=0; i<buttons.length; i++){
-        buttons[i].style.display = "none";
-    }
-    
-    //comparison to determines who won
+function playPaper(computerChoice) {
     if (computerChoice == 1) {
-        screen.src = 'images/tie.png';
-        feedback.innerHTML = "It's a tie. Reload the page and try again.";        
+        changeFeedback.innerHTML = "You win! Paper covers rock.";
     } else if (computerChoice == 2) {
-        screen.src = 'images/loser.gif';
-        feedback.innerHTML = "You lose. Paper covers rock.";  
+        changeFeedback.innerHTML =  "It's a tie. No points awarded.";            
     } else {
-        screen.src = 'images/winner.gif';
-        feedback.innerHTML = "You win! Rock beats scissors.";
+        changeFeedback.innerHTML = "You lose. Scissors cuts paper.";
     }
+}
 
-});
-
-//function for computer response when user chooses scissors
-var scissors_btn = document.getElementById("scissors_btn");
-scissors_btn.addEventListener("click", function(){
-    //hides choice buttons when response screen is displayed    
-    for (i=0; i<buttons.length; i++){
-        buttons[i].style.display = "none";
-    }
-    
-    //comparison to determines who won
+function playScissor(computerChoice) {
     if (computerChoice == 1) {
-        screen.src = 'images/tie.png';
-        feedback.innerHTML = "It's a tie. Reload the page and try again.";        
+        changeFeedback.innerHTML = "You lose. Rock beats scissors." ;        
     } else if (computerChoice == 2) {
-        screen.src = 'images/loser.gif';
-        feedback.innerHTML = "You lose. Paper covers rock.";  
+        changeFeedback.innerHTML = "You win! Scissors cuts paper.";  
+        computerScore += 1;
     } else {
-        screen.src = 'images/winner.gif';
-        feedback.innerHTML = "You win! Rock beats scissors.";
+        changeFeedback.innerHTML = "It's a tie. No points awarded.";
     }
+}
 
-});
+
+
+// //function for computer response when user chooses rock
+// var rock_btn = document.getElementById("rock_btn");
+// rock_btn.addEventListener("click", function(){
+        
+//     //comparison to determines who won
+//     if (computerChoice == 1) {
+//         changeFeedback.innerHTML = "It's a tie. No points awarded.";        
+//     } else if (computerChoice == 2) {
+//         changeFeedback.innerHTML = "You lose. Paper covers rock.";  
+//     } else {
+//         changeFeedback.innerHTML = "You win! Rock beats scissors.";
+//     }
+
+// });
+
+// //function for computer response when user chooses paper
+// var paper_btn = document.getElementById("paper_btn");
+// paper_btn.addEventListener("click", function(){
+        
+//     //comparison to determines who won
+//     if (computerChoice == 1) {
+//         changeFeedback.innerHTML = "It's a tie. No points awarded.";        
+//     } else if (computerChoice == 2) {
+//         changeFeedback.innerHTML = "You lose. Paper covers rock.";  
+//     } else {
+//         changeFeedback.innerHTML = "You win! Rock beats scissors.";
+//     }
+
+// });
+
+// //function for computer response when user chooses scissors
+// var scissors_btn = document.getElementById("scissors_btn");
+// scissors_btn.addEventListener("click", function(){
+        
+//     //comparison to determines who won
+//     if (computerChoice == 1) {
+//         changeFeedback.innerHTML = "It's a tie. No points awarded.";        
+//     } else if (computerChoice == 2) {
+//         changeFeedback.innerHTML = "You lose. Paper covers rock.";  
+//     } else {
+//         changeFeedback.innerHTML = "You win! Rock beats scissors.";
+//     }
+
+// });
